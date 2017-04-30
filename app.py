eab17f2ebe853fc8892d2fdd83fcf3984634708e
Nicholas Hubbard <nrhubbar@iupui.edu>
2017-04-30 19:22:26 -0400

    set up connection to redis

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import json
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+
+r = redis.StrictRedis(host="192.168.99.100")#redis
+
+
+def get(key):
+    return json(str(r.get(key)).split(" ")[1:])
 
 @app.route('/')
 def hello_world():
